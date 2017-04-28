ad1f414f2157cfb075835cd427cdab2857aa59b7
Ryan McAlpin <rhmcalpin@gmail.com>
2017-04-28 15:58:25 -0700

    saves questions with formatted date

--- app/components/ask-question.js
+++ app/components/ask-question.js
@@ -5,6 +5,17 @@ export default Ember.Component.extend({
   actions: {
     showForm: function() {
       this.set('askQuestion', true);
+    },
+    saveQuestion: function() {
+      var dt = new Date();
+      var params = {
+        question: this.get('question'),
+        author: this.get('author'),
+        notes: this.get('notes'),
+        timestamp: (dt.getMonth() + 1) + "/" + dt.getDate() + "/" + dt.getFullYear()
+      };
+      this.set('askQuestion', false);
+      this.sendAction('saveQuestion', params);
     }
   }
 });
