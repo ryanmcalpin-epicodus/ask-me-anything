3e290ec362c7903193da3b8424a72d275862ddbe
Ryan McAlpin <rhmcalpin@gmail.com>
2017-04-29 16:48:23 -0700

    add form validation

--- app/components/answer-question.js
+++ app/components/answer-question.js
@@ -7,18 +7,22 @@ export default Ember.Component.extend({
       this.set('answerQuestion', true);
     },
     saveAnswer() {
-      debugger;
-      var dt = new Date();
-      var params = {
-        author: this.get('author'),
-        answer: this.get('answer'),
-        timestamp: (dt.getMonth() + 1) + "/" + dt.getDate() + "/" + dt.getFullYear(),
-        question: this.get('question')
-      };
-      this.set('answerQuestion', false);
-      this.set('author', '');
-      this.set('answer', '');
-      this.sendAction('saveAnswer', params);
+      if (this.get('author')==="" || this.get('author')===undefined || this.get('answer')==="" || this.get('answer')===undefined) {
+        alert("Please fill out all of the fields.");
+      } else {
+        var dt = new Date();
+        alert(this.get('question'));
+        var params = {
+          author: this.get('author'),
+          answer: this.get('answer'),
+          timestamp: (dt.getMonth() + 1) + "/" + dt.getDate() + "/" + dt.getFullYear(),
+          question: this.get('question')
+        };
+        this.set('answerQuestion', false);
+        this.set('author', '');
+        this.set('answer', '');
+        this.sendAction('saveAnswer', params);
+      }
     },
     cancel() {
       this.set('answerQuestion', false);
--- app/components/ask-question.js
+++ app/components/ask-question.js
@@ -7,18 +7,22 @@ export default Ember.Component.extend({
       this.set('askQuestion', true);
     },
     saveQuestion: function() {
-      var dt = new Date();
-      var params = {
-        question: this.get('question'),
-        author: this.get('author'),
-        notes: this.get('notes'),
-        timestamp: (dt.getMonth() + 1) + "/" + dt.getDate() + "/" + dt.getFullYear()
-      };
-      this.set('askQuestion', false);
-      this.set('question', '');
-      this.set('author', '');
-      this.set('notes', '');
-      this.sendAction('saveQuestion', params);
+      if (this.get('question')==="" || this.get('author')==="" || this.get('notes')==="" || this.get('question')===undefined || this.get('author')===undefined || this.get('notes')===undefined) {
+        alert("Please fill out all of the fields.");
+      } else {
+        var dt = new Date();
+        var params = {
+          question: this.get('question'),
+          author: this.get('author'),
+          notes: this.get('notes'),
+          timestamp: (dt.getMonth() + 1) + "/" + dt.getDate() + "/" + dt.getFullYear()
+        };
+        this.set('askQuestion', false);
+        this.set('question', '');
+        this.set('author', '');
+        this.set('notes', '');
+        this.sendAction('saveQuestion', params);
+      }
     },
     cancel() {
       this.set('askQuestion', false);
